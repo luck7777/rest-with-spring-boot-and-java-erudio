783c4f51439420088b72a8a267912841d96affee
Lucas Araujo <lucascaraujo@outlook.com>
2023-04-12 10:01:23 -0300

    Implementacao dos verbos GET, PUT, POST e DELETE

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/com/lucascaraujo/services/PersonServices.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/com/lucascaraujo/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.lucascaraujo.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -25,5 +27,47 @@ public class PersonServices {
 		person.setGenero("Masculino");
 		
 		return person;
+	}
+	
+	public List<Person> findAll () {
+		
+		logger.info("Finding all peaple!");
+
+		List<Person> persons = new ArrayList<>();
+		for (int i = 1; i <= 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setNome("Nome Pessoa - " + i);
+		person.setSobrenome("Sobrenome Pessoa - " + i);
+		person.setEndereco("Endereço - " + i);
+		person.setGenero("Genero - " + i);
+		
+		return person;
+	}
+	
+	public Person create(Person person) {
+		
+		logger.info("Create one person!");
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("Update one person!");
+		
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+		logger.info("Deleting one person!");
 	}
 }
